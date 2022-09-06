41b0e2b46a2ebc36d1944edcbc5394d20a1e6e96
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2022-09-05 23:21:01 -0300

    Add files via upload

---
+++ Exercicio_010.py
@@ -0,0 +1,28 @@
+bordas = '\033[037m-=-\033[m' * 12
+print(bordas)
+print('{:^35}'.format('CONVERSOR DE MOEDAS'))
+print(bordas)
+print()
+real = float(input('Digite o valor (em reais) a ser convertido: R$ '))
+print(bordas)
+print('USD -> Para converter para dólar')
+print('EUR -> Para converter para euro')
+print('JPY -> Para converter para decâmetro')
+print(bordas)
+
+# Valores não atualizados
+
+while True:
+    convert = input('Digite a opção desejada: ').strip().upper()
+    match convert:
+        case 'USD':
+            print(f'\nO valor corresponde a ${(real/5.54):.2f}')
+            break
+        case 'EUR':
+            print(f'\nO valor corresponde a €{(real/6.34):.2f}')
+            break
+        case 'JPY':
+            print(f'\nO valor corresponde a ¥{(real/0.048):.2f}')
+            break
+        case _:
+            print('Por favor digite uma opção válida!')
